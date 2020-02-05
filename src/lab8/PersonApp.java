4d5b30c23e7215c0b85a271dc1ef162c2db2ed41
kanreuthai <kanreuthaisrichai40@gmail.com>
2020-02-05 11:48:49 +0700

    lab7,lab8

---
+++ src/lab8/PersonApp.java
@@ -0,0 +1,14 @@
+package lab8;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Student std1 = new Student("361211760034","Kanreuthai Srichai","STD001","Information Technology");
+        Person p1 =new Person("361211760033","Kanreuthai Srichai");
+
+        System.out.println(std1.getName());
+        System.out.println(p1.getName());
+
+
+
+    }
+}
